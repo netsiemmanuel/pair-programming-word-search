fa253420b13b1f964598358a0c6ca488f2ac6fb8
Netsanet <netsizebene@gmail.com>
2022-08-03 22:08:23 +0000

    fixing code and adding 1 test condition

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -8,11 +8,11 @@ describe("#wordSearch()", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['Y', 'F', 'Z', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'R', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'A', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'D', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'K', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
@@ -22,9 +22,9 @@ describe("#wordSearch()", function() {
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], // false 
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], //word is here  true  
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],// false 
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
@@ -35,4 +35,64 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+
+  it("should return false is blank input", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], // false 
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], //word is here  true  
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],// false 
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], []) // here<----- [] vs '' 
+
+    assert.isFalse(result);
+  });
+
+
+  
+
+
+  // it("should return does not work if there is a number in input", function() {
+  //   const result = wordSearch([
+  //     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], // false 
+  //     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], //word is here  true  
+  //     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],// false 
+  //     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  //     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'jeflee') // here<----- [] vs '' 
+
+
+  //   const assertEqual = function(actual, expected) {
+  //     if (actual === expected) {
+        
+  //       return true
+  //     } else {
+  //       return false
+        
+  //     }
+  //   };
+
+  //   assertEqual(result, 'does not work')
+
+
+    
+  // });
+
+
+
+
+
+
+
+
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,77 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (word == '' || word == []) {
+      return false
     }
-}
-
-module.exports = wordSearch
+  
+    // for (character of word) {
+    //   if (!isNaN){
+    //     return 'does not work'
+    //   }
+    // }
+  
+  
+  
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      //let status = false
+      for (l of horizontalJoin) {
+          if (l.includes(word)) {
+            return true
+          } 
+      }
+  
+      //put adjusted transpose here 
+  
+      const transpose = function (arry) {
+        // input is array of nested arrays
+        let fullArray = [];
+        let length = letters.length // [ ], [], []  < -- length of 3  
+      //[1, 2, 3, 4, 5, 6, 7]
+        for (let i= 0; i < length; i++) {  // loops through the number of arrays in the matrix and creats a blank aray for each. 
+          fullArray.push([])
+        }
+      
+        for (items of letters) {  // loops through matrix - each indiv array 
+      
+          for (let i= 0; i <  length; i++) { //loops through each indiv item(arry) in matrix
+            fullArray[i] += [items[i]] //pushes 
+          }
+        
+        }
+        //console.log(letters)
+        return fullArray;
+      };
+  
+      let verticalJoin = transpose(letters)
+  
+      for (l of verticalJoin) {
+        if (l.includes(word)) {
+          return true
+        } 
+      }
+  
+  
+      
+  
+      
+      return false
+      
+      
+      
+  }
+  
+  
+  console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], // false 
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], //word is here  true  
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],// false 
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], ''))
+  
+  
+  module.exports = wordSearch
+  
